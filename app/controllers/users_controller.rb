c49a8c81dad92e11fde9f9df243983ac54e8660e
carriesmith <carrie.elizabeth@gmail.com>
2015-04-24 20:48:00 -0400

    Signup form responsive and returns errors to invalid fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
+
+  # 
+  def show
+  	@user = User.find(params[:id])
+  	# debugger
   end
+
+  # routes.rb ---> get 'signup' => 'users#new'
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save 
+      # Handle a successful save
+    else
+      render 'new'
+    end
+  end # end create
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
