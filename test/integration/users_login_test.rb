9ef80e812c8e25a2e689457b76a9d3c1d40ba39b
carriesmith <carrie.elizabeth@gmail.com>
2015-04-27 15:27:51 -0400

    login tests created

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
   
+  def setup
+    @user = users(:michael)
+  end
+
   test "login with invalid information" do
     get login_path # Go to login screen
     assert_template 'sessions/new' # ASSERT: login screen renders
@@ -12,5 +16,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty? # ASSERT: Error not displayed on new screen
   end
 
+  test "login with valid information" do
+    get login_path
+    post login_path, session: { email: @user.email, password: 'password' }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
 
 end
