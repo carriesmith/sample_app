db4d8fe6a1a653d82500a8a9650ea35bfa5387dc
carriesmith <carrie.elizabeth@gmail.com>
2015-04-27 21:55:14 -0400

    remember me checkbox

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -37,6 +37,11 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+    
+    # Simulate user then hitting logout in another tab
+    delete logout_path
+    assert_redirected_to root_url, "redirect"
+
   end
 
 end
